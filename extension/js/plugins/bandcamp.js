e16f9f8d1b9292109bb1842844b0c4743b09a168
Jarek Radosz <jradosz@gmail.com>
2012-09-24 09:59:46 +0200

    Enable scrobbling from custom bandcamp domains.

--- extension/js/plugins/bandcamp.js
+++ extension/js/plugins/bandcamp.js
@@ -2,15 +2,21 @@
 
 	var plugin = scroblr.registerHost("bandcamp");
 
+	plugin.init = function () {
+		return this.hostre.test(document.location.hostname) ||
+			document.body.innerHTML.match(/src="http:\/\/bandcamp.com\/cd_ui"/);
+	};
+
 	plugin.scrape = function () {
 		var discover, info, isTrack, pageTitle;
 
-		discover = window.location.pathname.slice(1) === "discover";
 		info = {
 			stopped: !$(".inline_player .playbutton").hasClass("playing")
 		}
 
 		if (!info.stopped) {
+			discover = window.location.pathname.slice(1) === "discover";
+
 			if (discover) {
 				info.artist = $("#detail_body_container .itemsubtext a").text();
 			} else {
